1a829b808c4e1afde968646a8708cdea4f5e3070
martin <91570703+Marvian-programmer@users.noreply.github.com>
2022-01-27 14:59:36 -0700

    Email address to Phone number

--- login.js
+++ login.js
@@ -1,53 +1,54 @@
-function savetoken(token){
-// whatever passes as token should save into local storage
-    if (window.localStorage){
-     localStorage.setItem("token", token);
+function savetoken(token) {
+    // whatever passes as token should save into local storage
+    if (window.localStorage) {
+        localStorage.setItem("token", token);
     }
 
 }
 
-function checkexpiredtoken(){
-// read token from local storage - check with ajax call
-    if(window.localStorage){
-    usertoken = localStorage.getItem("token", token);
-    $.ajax({
-       type: 'GET',
-        url: '/checkToken',
-        data: '{"usertoken":"' + usertoken + '"}',
-        success: function(data){savetoken(data)},
-        contentType: "application/text",
-        dataType: 'text' })
+function checkexpiredtoken() {
+    // read token from local storage - check with ajax call
+    if (window.localStorage) {
+        usertoken = localStorage.getItem("token", token);
+        $.ajax({
+            type: 'GET',
+            url: '/checkToken',
+            data: '{"usertoken":"' + usertoken + '"}',
+            success: function(data) { savetoken(data) },
+            contentType: "application/text",
+            dataType: 'text'
+        })
     }
 }
 
-function userlogin(){
+function userlogin() {
     setuserpassword();
     setusername();
     $.ajax({
         type: 'POST',
         url: '/login',
-        data: ' {"userName":"'+ userName +'", "password":"'+ password +'"}', // or JSON.stringify ({name: 'jonas'}),
+        data: ' {"userName":"' + userName + '", "password":"' + password + '"}', // or JSON.stringify ({name: 'jonas'}),
         success: function(data) {
             savetoken(data);
             localStorage.removeItem("customer");
             window.location.href = "/timer.html";
-         },
+        },
         contentType: "application/text",
         dataType: 'text'
     });
 
 }
 
-function setusername(){
+function setusername() {
     userName = $("#un").val();
 }
 
-function setuserpassword(){
+function setuserpassword() {
     password = $("#pw").val();
 }
 
-var enterFunction = (event) =>{
-    if (event.keyCode === 13){
+var enterFunction = (event) => {
+    if (event.keyCode === 13) {
         event.preventDefault();
         $("#loginbtn").click();
     }
